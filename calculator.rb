565335f298ff46fe451e609d94e167f4f3a208cc
Danielle Giglia <daniellegiglia@yahoo.com>
2015-06-23 00:50:19 +0000

    Revise while loop

--- calculator.rb
+++ calculator.rb
@@ -1,14 +1,8 @@
-=begin
-throw error if they choose an invalid operation
-convert all num strings to floats to do more complex operations
-throw error if they don't enter a number
-=end
-
 def prompt(msg)
   puts "*** #{msg} ***"
 end
 
-prompt("Welcome to Calcutron2000. I can do wonders with two non-zero numbers! What operation can I perform for you today? Please select 'A' for add, 'S' for subtract, 'M' for multiply, or 'D' for divide.")
+prompt("Welcome to Calcutron2000. I can do wonders with two non-zero numbers! What operation can I perform for you today? \n Please select 'A' for add, 'S' for subtract, 'M' for multiply, or 'D' for divide.")
 operator = gets.chomp.downcase
 
 case operator
@@ -22,14 +16,14 @@ end
   
 prompt("Great idea! Let's #{do_something}! What is the first number you'd like to #{do_something}?")
   num1 = gets.chomp
-    if num1.to_f == 0.0
+    while num1.to_f == 0.0
       prompt("Try again with a non-zero number!")
       num1 = gets.chomp
     end
     
 prompt("What is the second number you'd like to #{do_something}?")
   num2 = gets.chomp
-    if num2.to_f == 0.0
+    while num2.to_f == 0.0
       prompt("Try again with a non-zero number!")
       num2 = gets.chomp
     end
