75355100fe90354b3149d20c0c8fdc037001589b
Danielle Giglia <daniellegiglia@yahoo.com>
2015-06-23 01:02:22 +0000

    Revise prompts

--- calculator.rb
+++ calculator.rb
@@ -2,19 +2,24 @@ def prompt(msg)
   puts "*** #{msg} ***"
 end
 
-prompt("Welcome to Calcutron2000. I can do wonders with two non-zero numbers! What operation can I perform for you today? \n Please select 'A' for add, 'S' for subtract, 'M' for multiply, or 'D' for divide.")
+prompt("Welcome to Calcutron2000. I can do wonders with two non-zero numbers! What operation can I perform for you today?")
+prompt("Please select 'A' for add, 'S' for subtract, 'M' for multiply, or 'D' for divide.")
 operator = gets.chomp.downcase
+
+until ['a', 's', 'm', 'd'].include?(operator)
+  prompt("Invalid selection! Please try again.")
+  operator = gets.chomp.downcase
+end
 
 case operator
   when 'a' then do_something = "add"
   when 's' then do_something = "subtract"
   when 'm' then do_something = "multiply"
   when 'd' then do_something = "divide"
-  else 
-    raise ArgumentError, "Invalid input! You'll need a Calcutron3000 for that operation!"
 end
-  
-prompt("Great idea! Let's #{do_something}! What is the first number you'd like to #{do_something}?")
+    
+prompt("Great idea! Let's #{do_something}!") 
+prompt("What is the first number you'd like to #{do_something}?")
   num1 = gets.chomp
     while num1.to_f == 0.0
       prompt("Try again with a non-zero number!")
